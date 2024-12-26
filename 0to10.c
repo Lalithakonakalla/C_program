e5c83c93a08ea5679847c45be3d08aef35e98a85
Lalithakonakalla <lalithakonakalla14@gmail.com>
2024-12-26 12:03:44 +0530

    Create 0to10.c

---
+++ 0to10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    int i,n;
+    printf("enter the n value :");
+    scanf("%d",&n);
+    for (int i=0;i<=10;i++)
+    {
+        printf("%d%",i);
+        
+    }
+    {
+        printf("/n");
+    }
+    for (int i=10;i>=0;i--)
+    {
+        printf("%d",i);
+    }
+}
