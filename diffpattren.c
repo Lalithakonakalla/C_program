d579f5d3fe06dac95c09b3413a3df37c72ba04de
Lalithakonakalla <lalithakonakalla14@gmail.com>
2024-12-30 12:26:10 +0530

    diffpattren.c

---
+++ diffpattren.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    for (i=0;i<=5;i++)
+    {
+       
+        for (k=0;k<=i;k++)
+     {
+         printf ("*");
+     }
+         printf ("\n");
+     }
+
+    for (i=0;i<5;++i)
+    {
+        for (j=i;j<5;++j)
+     printf("*");
+     {
+         printf ("\n");
+     }
+}
+}
